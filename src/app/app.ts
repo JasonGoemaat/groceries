9086f7db517982a6104c529f2d5feb46326217e7
Jason <jason.goemaat@gmail.com>
2026-04-02 14:01:47 -0500

    Moved header to app, working a bit on about page

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 import { Theme } from 'flowbite-angular/theme-toggle';
+import { HeaderComponent } from '@src/app/header-component/header-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
   hostDirectives: [Theme],
